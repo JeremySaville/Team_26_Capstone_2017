c4ebabcf2e3e4f8e04ed8b46910d65b7b735bb0a
JeremySaville <jsaville@internode.on.net>
2017-08-02 19:41:55 +1000

    username

--- DiabetesApp/DiabetesApp/DataTypes/User.cs
+++ DiabetesApp/DiabetesApp/DataTypes/User.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace DiabetesApp.DataTypes{
     public class User{
         public int gamified { get; set; }
+        public string username { get; set; }
         public string birthday { get; set; }
         public string phone { get; set; }
         public string email { get; set; }
