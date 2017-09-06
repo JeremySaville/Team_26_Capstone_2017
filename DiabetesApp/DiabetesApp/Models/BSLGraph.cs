4b225d71c058ad8ce813274a2585072168d71c08
Evelyne Berger-Uichanco <evelyne.rose@ymail.com>
2017-09-06 20:46:13 +1000

    Started working on analytics

---
+++ DiabetesApp/DiabetesApp/Models/BSLGraph.cs
@@ -0,0 +1,58 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DiabetesApp.Models {
+    public class BSLGraph {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PlotModel BSLModel { get; set; }
+
+        public BSLGraph(DateTime start, DateTime end) {
+            BSLModel = CreateBSLChart(start, end);
+        }
+
+        private PlotModel CreateBSLChart(DateTime start, DateTime end) {
+            PlotModel model = new PlotModel { Title = "BSL Levels 27 May 2017" };
+            double earliestDate = DateTimeAxis.ToDouble(DateTime.Parse("Sat 27 May 2017 08:23:00"));
+            double latestDate = DateTimeAxis.ToDouble(DateTime.Parse("Sat 27 May 2017 19:48:00"));
+
+            var highBSSeries = new AreaSeries();
+            highBSSeries.Points.Add(new DataPoint(earliestDate, 9));
+            highBSSeries.Points.Add(new DataPoint(latestDate, 9));
+            highBSSeries.Points.Add(new DataPoint(latestDate, 15));
+            highBSSeries.Points.Add(new DataPoint(earliestDate, 15));
+            highBSSeries.Points.Add(new DataPoint(earliestDate, 9));
+            highBSSeries.Color = OxyColor.FromRgb(255, 128, 128);
+
+            var lowBSSeries = new AreaSeries();
+            lowBSSeries.Points.Add(new DataPoint(earliestDate, 5));
+            lowBSSeries.Points.Add(new DataPoint(latestDate, 5));
+            lowBSSeries.Points.Add(new DataPoint(latestDate, -1));
+            lowBSSeries.Points.Add(new DataPoint(earliestDate, -1));
+            lowBSSeries.Points.Add(new DataPoint(earliestDate, 5));
+            lowBSSeries.Color = OxyColor.FromRgb(255, 163, 102);
+
+            var lineSeries = new LineSeries();
+            lineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Parse("Sat 27 May 2017 08:23:00")), 8));
+            lineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Parse("Sat 27 May 2017 10:14:00")), 11));
+            lineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Parse("Sat 27 May 2017 13:04:00")), 7));
+            lineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Parse("Sat 27 May 2017 17:22:00")), 9));
+            lineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Parse("Sat 27 May 2017 19:48:00")), 6));
+            lineSeries.Color = OxyColor.FromRgb(0, 61, 153);
+
+            model.Series.Add(highBSSeries);
+            model.Series.Add(lowBSSeries);
+            model.Series.Add(lineSeries);
+
+            model.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom, AbsoluteMinimum = earliestDate, AbsoluteMaximum = latestDate, Title = "DateTime" });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, AbsoluteMinimum = 0, AbsoluteMaximum = 14, Title = "BSL" });
+            model.PlotAreaBackground = OxyColor.FromRgb(255, 255, 255);
+            return model;
+        }
+    }
+}
--- DiabetesApp/DiabetesApp/Pages/AnalyticsPage.xaml.cs
+++ DiabetesApp/DiabetesApp/Pages/AnalyticsPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Firebase.Xamarin.Auth;
+﻿using DiabetesApp.Models;
+using Firebase.Xamarin.Auth;
+using Firebase.Xamarin.Database;
+using Firebase.Xamarin.Database.Query;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +12,64 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace DiabetesApp
-{
+namespace DiabetesApp {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AnalyticsPage : ContentPage
-	{
-		public AnalyticsPage (FirebaseAuthLink auth)
-		{
-			InitializeComponent ();
-		}
-	}
+	public partial class AnalyticsPage : ContentPage {
+
+        FirebaseAuthLink auth;
+        private const string FirebaseURL = "https://diabetesarp.firebaseio.com/";
+        private ObservableCollection<LogbookListItem> logs;
+
+        //Constructor
+        public AnalyticsPage (FirebaseAuthLink auth) {
+            InitializeComponent();
+            logs = new ObservableCollection<LogbookListItem>();
+            refreshGraph();
+        }
+
+        private void refreshGraph() {
+            
+        }
+
+        //When the page is navigated back to 
+        protected override async void OnAppearing() {
+            await updateEntries();
+        }
+
+        //Method to update the entries list
+        async Task updateEntries() {
+            try {
+                var firebase = new FirebaseClient(FirebaseURL);
+                var items = await firebase
+                    .Child("logbooks")
+                    .Child(auth.User.LocalId)
+                    .OrderByKey()
+                    .WithAuth(auth.FirebaseToken)
+                    .OnceAsync<DataTypes.LogbookEntry>();
+
+                logs.Clear();
+                foreach (var item in items) {
+                    LogbookListItem l = new LogbookListItem();
+                    //Create strings for the listview
+                    l.title = DateTime.Parse(item.Key).ToString("dddd d MMMM yyyy, h:mm tt");
+                    l.description = "Blood Glucose: " + item.Object.BG.ToString() + "  Carb Exchange: " + item.Object.carbEx.ToString();
+                    //Add in the rest of the log entry information
+                    l.BG = item.Object.BG;
+                    l.BI = item.Object.BI;
+                    l.carbCorrect = item.Object.carbCorrect;
+                    l.carbEx = item.Object.carbEx;
+                    l.comments = item.Object.comments;
+                    l.entryTime = DateTime.Parse(item.Key);
+                    l.mood = item.Object.mood;
+                    l.QA = item.Object.QA;
+                    l.qaCorrect = item.Object.qaCorrect;
+                    l.updateTime = DateTime.Parse(item.Object.updateTime);
+                    //add to list
+                    logs.Add(l);
+                }
+            } catch {
+                await DisplayAlert("Error updating entries", "Unable to update log entries list at this time", "OK");
+            }
+        }
+    }
 }
