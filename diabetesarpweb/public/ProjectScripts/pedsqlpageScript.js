84e18fe6b3e3f484c916ee3591e0aad769ef4303
JeremySaville <jsaville@internode.on.net>
2017-09-18 12:27:31 +1000

    minor changes, page skeleton

--- diabetesarpweb/public/ProjectScripts/pedsqlpageScript.js
+++ diabetesarpweb/public/ProjectScripts/pedsqlpageScript.js
@@ -12,35 +12,12 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-function writeUserData(uid/*, username, name, phone, dob, hba1c*/) {
-    var uid = document.getElementById('uuidinput').value;
-    var name = document.getElementById('InputName').value;
-    var phone = document.getElementById('InputPhone').value;
-    var dob = document.getElementById('InputDOB').value;
-    var hba1c = document.getElementById('InputHBA1C').value;
-    firebase.database().ref('users/' + uid).set({
-        username: name,
-        phone: phone,
-        dob: dob,
-        HBA1C: hba1c
-    });
-}
-
-//function stepForwardToggle(){
-	//document.getElementById('next').disabled = false;
-//}
-
-function stepForward() {
-    
-    window.location = "quizpage.html";
-    
-}
 
 function userDetails() {
     // Listening for auth state changes.
     // [START authstatelistener]
     firebase.auth().onAuthStateChanged(function (user) {
-        
+
         if (user) {
             // User is signed in.
             var displayName = user.displayName;
@@ -51,33 +28,28 @@ function userDetails() {
             var uid = user.uid;
             var providerData = user.providerData;
             // [Fill textarea and input field from user details]
-            
-            document.getElementById('uuidinput').defaultValue = uid;
+
+            //document.getElementById('uuidinput').defaultValue = uid;
             document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
             //document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
             document.getElementById('quickstart-account-details').textContent = email + ' ' + uid;
-            
+
         } else {
             // User is signed out.
             document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
             document.getElementById('quickstart-account-details').textContent = 'null';
-            window.alert("Please Login before entering your details");
+            window.alert("Please Login before completing your quiz");
             window.location = 'index.html'
-            
+
         }
-        
+
     });
     // [END authstatelistener]
     //event listener for submitting data
-    document.getElementById('submit').addEventListener('click', writeUserData, false);
-	//document.getElementById('submit').addEventListener('click', stepForward, false);
-    
+    document.getElementById('submit').addEventListener('click', check, false);
+    //document.getElementById('submit').addEventListener('click', stepForward, false);
+
 }
-
-
-//function fillUid() {
-
-//}
 
 window.onload = function () {
     userDetails();
--- diabetesarpweb/public/ProjectScripts/quizpageScript.js
+++ diabetesarpweb/public/ProjectScripts/quizpageScript.js
@@ -12,20 +12,59 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+function writeUserData(uid, username, name, phone, dob, hba1c) {
+    var uid = document.getElementById('uuidinput').value;
+    var name = document.getElementById('InputName').value;
+    var phone = document.getElementById('InputPhone').value;
+    var dob = document.getElementById('InputDOB').value;
+    var hba1c = document.getElementById('InputHBA1C').value;
+    firebase.database().ref('users/' + uid).set({
+        username: name,
+        phone: phone,
+        dob: dob,
+        HBA1C: hba1c
+    });
+}
+
+function writeUserData(uid, username, name, phone, dob, hba1c) {
+    var uid = document.getElementById('uuidinput').value;
+    var name = document.getElementById('InputName').value;
+    var phone = document.getElementById('InputPhone').value;
+    var dob = document.getElementById('InputDOB').value;
+    var hba1c = document.getElementById('InputHBA1C').value;
+    firebase.database().ref('users/' + uid).set({
+        username: name,
+        phone: phone,
+        dob: dob,
+        HBA1C: hba1c
+    });
+}
+
 //document.getElementById('submit').addEventListener('click', submitQuiz, false);
 
 //function submitQuiz() {
 
 //}
-
-//var numQuestions = 23;
+//var answerArray = new Array();
+//function check() {
+//    var answers = document.forms[0];
+//    var i;
+//    for (i = 0; i < answers.length; i++) {
+//        if (answers[i].checked) {
+//            answerArray.push(answerArray[i].value)
+//        }
+//    }
+//}
+////var numQuestions = 22;
+//var numQuestions = document.getElementById('optradio');
 //var test = new Array();
-// following two functions only return a single value repeated in the array
-//function getResults() {
+//// following two functions only return a single value repeated in the array
+//function getResults(quizForm1) {
 //    var radios = document.getElementById('quizForm1');
 //    for (i = 0; i < radios.length; i++) {
 //        if (radios[i].checked) {
 //            return radios[i].value;
+
 //        }
 //    }
 //    return null;
@@ -34,34 +73,34 @@ var database = firebase.database();
 //function check() {
 //    for (var i = 0; i <= numQuestions; i++) {
 //        //console.log(i,getCheckedValue('Q'+i));
-//        test[i] = getResults('Q' + i);
+//        test[i] = getResults('optradio' + i);
 //    }
 //    //console.log(test);
 //    document.getElementById('quickstart-account-details').textContent = test;
 //}
 
-var pubNumbers = 23
-var test = new Array();
-function getResults() {
-    var numQuestions = document.getElementsById('optradio').length;
-    var radios = document.getElementsById('optradio');
-    for (i = 0; i < numQuestions; i++) {
-        if (radios[i].checked) {
-            return radios[i].value;
+//var pubNumbers = 23
+//var test = new Array();
+//function getResults() {
+//    var numQuestions = document.getElementsById('optradio').length;
+//    var radios = document.getElementsById('optradio');
+//    for (i = 0; i < numQuestions; i++) {
+//        if (radios[i].checked) {
+//            return radios[i].value;
 
-        }
-    }
-    return null;
-}
+//        }
+//    }
+//    return null;
+//}
 
-function check() {
-    for (var i = 1; i <= pubNumbers; i++) {
-        //console.log(i,getCheckedValue('Q'+i));
-        test[i - 1] = getResults('Q' + i);
-    }
-    //console.log(test);
-    document.getElementById('quickstart-account-details').textContent = test;
-}
+//function check() {
+//    for (var i = 1; i <= pubNumbers; i++) {
+//        //console.log(i,getCheckedValue('Q'+i));
+//        test[i - 1] = getResults('Q' + i);
+//    }
+//    //console.log(test);
+//    document.getElementById('quickstart-account-details').textContent = test;
+//}
 
 function userDetails() {
     // Listening for auth state changes.
@@ -82,7 +121,7 @@ function userDetails() {
             //document.getElementById('uuidinput').defaultValue = uid;
             document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
             //document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-            //document.getElementById('quickstart-account-details').textContent = email + ' ' + uid;
+            document.getElementById('quickstart-account-details').textContent = email + ' ' + uid;
 
         } else {
             // User is signed out.
