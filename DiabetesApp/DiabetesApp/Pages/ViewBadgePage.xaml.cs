936b7b0a48a22b282b2ffef2e6b934dfa950d9ca
Evelyne Berger-Uichanco <evelyne.rose@ymail.com>
2017-10-23 14:59:49 +1000

    Fixed merge errors, should all work now

--- DiabetesApp/DiabetesApp/Pages/ViewBadgePage.xaml.cs
+++ DiabetesApp/DiabetesApp/Pages/ViewBadgePage.xaml.cs
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-﻿using DiabetesApp.Models;
+using DiabetesApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,56 +22,4 @@ namespace DiabetesApp.Pages {
             Navigation.PopModalAsync();
         }
 	}
-=======
-﻿using DiabetesApp.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace DiabetesApp.Pages {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ViewBadgePage : ContentPage {
-		public ViewBadgePage (string badgeKey) {
-			InitializeComponent ();
-            badgeName.Text = BadgeList.getBadgeName(badgeKey);
-            badgeImage.Source = BadgeList.getBadgeLink(badgeKey);
-            badgeDescription.Text = BadgeList.getBadgeDescription(badgeKey);
-		}
-
-        public void onClick_backButton(object sender, EventArgs e) {
-            Navigation.PopModalAsync();
-        }
-	}
->>>>>>> 0d9259108b810199bfe41e5545b95dc2162cf056
-=======
-﻿using DiabetesApp.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace DiabetesApp.Pages {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ViewBadgePage : ContentPage {
-		public ViewBadgePage (string badgeKey) {
-			InitializeComponent ();
-            badgeName.Text = BadgeList.getBadgeName(badgeKey);
-            badgeImage.Source = BadgeList.getBadgeLink(badgeKey);
-            badgeDescription.Text = BadgeList.getBadgeDescription(badgeKey);
-		}
-
-        public void onClick_backButton(object sender, EventArgs e) {
-            Navigation.PopModalAsync();
-        }
-	}
->>>>>>> a96ed9bc9938ff2c92b2f6669ec9509c04c5b753
 }
