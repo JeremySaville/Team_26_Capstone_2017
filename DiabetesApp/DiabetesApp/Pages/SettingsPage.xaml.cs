85562d955cd23aba0810a1e3a78ae756d38214b6
Evelyne Berger-Uichanco <evelyne.rose@ymail.com>
2017-10-31 12:56:13 +1000

    Visual improvements, standardised title style, added logout button to settings page

--- DiabetesApp/DiabetesApp/Pages/SettingsPage.xaml.cs
+++ DiabetesApp/DiabetesApp/Pages/SettingsPage.xaml.cs
@@ -8,14 +8,20 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace DiabetesApp
-{
+namespace DiabetesApp { 
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class SettingsPage : ContentPage
-	{
-		public SettingsPage (FirebaseAuthLink auth)
-		{
+	public partial class SettingsPage : ContentPage { 
+
+        FirebaseAuthLink auth;
+
+		public SettingsPage (FirebaseAuthLink auth) {
 			InitializeComponent ();
+            this.auth = auth;
 		}
-	}
+
+        private async void onClick_logoutButton(object sender, EventArgs e) {
+            bool logout = await DisplayAlert("Log Out?", "Log out of the app?", "Yes", "No");
+            if(logout) Application.Current.MainPage = new LoginPage();
+        }
+    }
 }
